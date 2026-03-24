2d9d367f1e23d483c8b0e0748587e793b90421e3
Raghib Haque <raghib.haque.2007@gmail.com>
2026-03-24 22:09:04 +0000

    Added a prediction function that passes the hours studied, and pass/fail rate into the prediction function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def forward(X, W, b):
+    z = X @ W + b
+    a = sigmoid(z)
+    return a
+
 X = np.array([[1], [2], [3], [4], [5]])
 
 y = np.array([[0], [0], [0], [1], [1]])
@@ -8,3 +16,6 @@ np.random.seed(42)
 
 W = np.random.randn(1, 1)  # weight
 b = np.zeros((1, 1))       # bias
+
+y_pred = forward(X, W, b)
+print(y_pred)
